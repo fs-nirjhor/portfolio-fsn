cd818a1e64dbd246cc2bc8d981247821535e3a63
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-03-31 15:15:11 +0600

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,38 +7,97 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section id="home" className="min-h-[85vh] lg:min-h-[78vh] flex items-center">
+    <section
+      id="home"
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+    >
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-8  lg:flex-row lg:items-center lg:gap-x-12">
-      {/* text */}
+          {/* text */}
           <div className="flex-1 font-secondary text-center lg:text-left ">
-            <h1 className="text-[55px] lg:text-[110px] font-bold leading-[0.8]">FS <span>NIRJHOR</span></h1>
-          <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
-            <span className="text-white mr-4">I am a</span>
-            <TypeAnimation sequence={['Developer', 2000, 'Designer', 2000, 'Youtuber', 2000]}
-            speed={50}
-            className="text-accent"
-            wrapper="span"
-            repeat= {Infinity}
-            />
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] lg:text-[110px] font-bold leading-[0.8]"
+            >
+              FS <span>NIRJHOR</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+            >
+              <span className="text-white mr-4">I am a</span>
+              <TypeAnimation
+                sequence={[
+                  "Developer",
+                  2000,
+                  "Designer",
+                  2000,
+                  "Youtuber",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 mx-w-lg mx-auto lg:mx-0"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit,
+              ducimus.
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <button className="btn btn-lg"> Contact me</button>
+              <a href="/" className="btn-link text-gradient">
+                My Portfolio
+              </a>
+            </motion.div>
+            {/* socials */}
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
+              <a href="/">
+                <FaYoutube />
+              </a>
+              <a href="/">
+                <FaGithub />
+              </a>
+              <a href="/">
+                <FaDribbble />
+              </a>
+            </motion.div>
           </div>
-          <p className="mb-8 mx-w-lg mx-auto lg:mx-0" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, ducimus.</p>
-          <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
-          <button className="btn btn-lg"> Contact me</button>
-          <a href="/" className="btn-link text-gradient">My Portfolio</a>
-          </div>
-          {/* socials */}
-          <div className="flex text-[20] gap-x-6 max-w-max mx-auto lg:mx-0">
-            <a href="/"><FaYoutube/></a>
-            <a href="/"><FaGithub/></a>
-            <a href="/"><FaDribbble/></a>
-          </div>
+          {/* Avatar */}
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] "
+          >
+            <img src={Avatar} alt="avatar" />
+          </motion.div>
         </div>
-        {/* Avatar */}
-        <div>
-          <img src={Avatar} alt="avatar" />
-        </div>
-          </div>
       </div>
     </section>
   );
