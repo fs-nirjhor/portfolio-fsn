2e2bb3a34d36955426429a21a56ec1dce009965a
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-03-31 14:20:30 +0600

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React from "react";
+import Avatar from "../assets/avatar.svg";
+import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section id="home" className="min-h-[85vh] lg:min-h-[78vh] flex items-center">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8  lg:flex-row lg:items-center lg:gap-x-12">
+      {/* text */}
+          <div className="flex-1 font-secondary text-center lg:text-left ">
+            <h1 className="text-[55px] lg:text-[110px] font-bold leading-[0.8]">FS <span>NIRJHOR</span></h1>
+          <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+            <span className="text-white mr-4">I am a</span>
+            <TypeAnimation sequence={['Developer', 2000, 'Designer', 2000, 'Youtuber', 2000]}
+            speed={50}
+            className="text-accent"
+            wrapper="span"
+            repeat= {Infinity}
+            />
+          </div>
+          <p className="mb-8 mx-w-lg mx-auto lg:mx-0" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, ducimus.</p>
+          <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+          <button className="btn btn-lg"> Contact me</button>
+          <a href="/" className="btn-link text-gradient">My Portfolio</a>
+          </div>
+          {/* socials */}
+          <div className="flex text-[20] gap-x-6 max-w-max mx-auto lg:mx-0">
+            <a href="/"><FaYoutube/></a>
+            <a href="/"><FaGithub/></a>
+            <a href="/"><FaDribbble/></a>
+          </div>
+        </div>
+        {/* Avatar */}
+        <div>
+          <img src={Avatar} alt="avatar" />
+        </div>
+          </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
